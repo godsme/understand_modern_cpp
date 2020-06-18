2ca6b63e5a60ad107cec8a78f18c38f31527404f
Darwin Yuan <darwin_yuan@hotmail.com>
2020-06-18 02:50:52 -0700

    refactored

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,12 +7,12 @@ import os
 
 # -- Project information -----------------------------------------------------
 
-project = 'TRANSACTION DSL'
+project = 'Understanding Modern C++'
 copyright = '2020, Darwin Yuan'
 author = 'Darwin Yuan'
 
 # The short X.Y version
-version = '2.0'
+version = '0.1'
 # The full version, including alpha/beta/rc tags
 release = '1'
 
